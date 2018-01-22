32a8e39576973939c893f8cd7a1642f55bacbd4d
AHosseinkhani <ahosseinkhani@coredigital.com>
2018-01-22 07:59:56 -0800

    bug fix

--- index.js
+++ index.js
@@ -47,12 +47,17 @@ cmd.command('gen-dml')
     prompt(questions).then((answers) => {
       console.log(answers);
       let fileName = genFileName(answers.ticketNumber, answers.newPresName);
+      let dueDate = answers.dueDate;
+      if(!new Date(dueDate)) throw new Error('Due date is not a valid date!');
+      let dueYear = new Date(dueDate).getfullYear();
       fs.readFile('./assets/dml.txt', 'utf8', (err, data) => {
-        data = data.replace('{{ticketNumber}}', answers.ticketNumber);
+        data = data.replace(/{{ticketNumber}}/g, answers.ticketNumber);
         data = data.replace('{{dueDate}}', answers.dueDate);
+        data = data.replace('{{dueYear}}', dueYear);
         data = data.replace('{{username}}', answers.username);
         data = data.replace('{{templatePresId}}', answers.templatePresId);
         data = data.replace('{{newpresId}}', answers.newpresId);
+        data = data.replace(/{{newPresName}}/g, answers.newPresName);
         data = data.replace('{{fileName}}', fileName);
         fs.writeFile(`${fileName}.sql`, data, (err) => {
           if(err) throw err;
